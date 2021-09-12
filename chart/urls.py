e7bcf54c838ad51f33a2a71104486df71be05e8d
AxelHowe <75995668+AxelHowe@users.noreply.github.com>
2021-09-12 13:10:18 +0800

    add secret export .csv

--- chart/urls.py
+++ chart/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('export_all', views.export_all, name="export_all"),
     path('export_NP', views.export_NP, name="export_NP"),
     path('export_M', views.export_M, name="export_M"),
+    path('export_secret', views.export_secret, name="export_secret"),
 ]
--- chart/views.py
+++ chart/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.utils.http import urlquote
 import csv, codecs
-from join.models import Member
+from join.models import Member, secret
 
 def joinclub(request):
     return render(request, 'joinclub.html', {})
@@ -48,4 +48,18 @@ def export_NP(request):
     writer.writerow(['姓名', '學號', '系級', '年級', '手機號碼', '電子郵件', '入社狀態'])
     for member in members:
         writer.writerow([member.name, member.nid, member.dept, member.level, member.phone, member.email, member.get_status_display()])
+    return response
+
+
+@login_required
+def export_secret(request):
+    members = secret.objects.all()
+    response = HttpResponse(content_type='text/csv')
+    response.write(codecs.BOM_UTF8)
+    response['Content-Disposition'] = 'attachment; filename="%s"' % (
+        urlquote("保密協議名單.csv"))
+    writer = csv.writer(response)
+    writer.writerow(['姓名', '學號', '手機號碼'])
+    for member in members:
+        writer.writerow([member.name, member.nid, member.phone])
     return response
