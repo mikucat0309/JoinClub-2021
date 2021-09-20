98bd3d5cc3bf3f4a82257acc3758051e3ccee41b
cocox33x <cocox33x@gmail.com>
2021-09-20 17:01:05 +0800

    更新 coming soon、部分社服、注意事項、保密協議

--- join/models.py
+++ join/models.py
@@ -29,10 +29,13 @@ class Member(models.Model):
 
     CLOTHES_CHOICES = [
         ('N', '不購買'),
-        ('S', '尺寸S'),
-        ('M', '尺寸M'),
-        ('L', '尺寸L'),
-        ('XL', '尺寸XL'),
+        ('XXS', '尺寸 XXS'),
+        ('XS', '尺寸 XS'),
+        ('S', '尺寸 S'),
+        ('M', '尺寸 M'),
+        ('L', '尺寸 L'),
+        ('XL', '尺寸 XL'),
+        ('2XL', '尺寸 2XL'),
     ]
 
     CLOTHES_STATUS_CHOICES = [
@@ -48,13 +51,12 @@ class Member(models.Model):
     # 不能用 unique=True，因為空字串也會被判斷成一樣
     phone = models.CharField(max_length=15, null=True, unique=True)
     email = models.EmailField(unique=True)
-    status = models.CharField(
-        max_length=2, choices=STATUS_CHOICES, default="NP")
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default="NP")
     pay = models.CharField(max_length=2, choices=PAY_CHOICES, default="C")
     bankAccount = models.CharField(max_length=5, null=True, blank=True)
     is_FCU = models.CharField(max_length=5, choices=SCHOOL_CHOICES,default="Y")
     school = models.CharField(max_length=50, blank=True)
-    DiscordId = models.CharField(max_length=50, blank=True)
+    DiscordId = models.CharField(max_length=50)
     clothes = models.CharField(max_length=4, choices=CLOTHES_CHOICES, default='N')
     clothes_status = models.CharField(max_length=3, choices=CLOTHES_STATUS_CHOICES, default='N')
     def __str__(self):
@@ -67,8 +69,7 @@ class secret(models.Model):
     """
     name = models.CharField(max_length=50)
     nid = models.CharField(max_length=15, unique=True)
-    phone = models.CharField(max_length=15, null=True,
-                             unique=True)
+    phone = models.CharField(max_length=15, null=True, unique=True)
     create_time = models.DateTimeField(auto_now_add=True)
 
 class receipt(models.Model):
